8fd6ce8d37f2829feb6808613dd55c1082f4a56a
vaishnavihole <vaishnavihole1@gmail.com>
2023-02-23 10:25:39 +0530

    use fsmodule readFileSync

--- fsmodule.js
+++ fsmodule.js
@@ -1,4 +1,11 @@
 const fs = require('fs');
-fs.readFile('file.txt', 'utf8', (err, data)=>{
-    console.log(err, data);
-})
+
+// fs.readFile('file.txt', 'utf8', (err, data)=>{
+//     console.log(err, data);
+// })
+
+const a = fs.readFileSync('file.txt')
+console.log(a);
+
+console.log("Finished reading file");
+
