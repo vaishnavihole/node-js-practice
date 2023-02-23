c25a99210263d96dec3d07ce2b82096e6d84ef65
vaishnavihole <vaishnavihole1@gmail.com>
2023-02-23 09:35:19 +0530

    use path extname

--- pathmodule.js
+++ pathmodule.js
@@ -4,3 +4,5 @@ const a1 = path.basename('C:\\temp||myfile.html');
 const a2 = path.dirname('C:\\temp||myfile.html');
 console.log(a1);
 console.log(a2);
+const a3 = path.extname(__filename)
+console.log(a3);
