4c12f8c59a0996c21494c0790613b27eea5ed364
vaishnavihole <vaishnavihole1@gmail.com>
2023-02-24 11:34:55 +0530

    add /about

--- website.js
+++ website.js
@@ -3,11 +3,17 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res)=>{
-    console.log(req);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html')
-    res.end('<h1> This is Harry</h1> <p> Hey this is the way to rock the world! </p>');
+    console.log(req.url)
+    if(req.url == '/'){
+        res.end('<h1> This is Harry</h1> <p> Hey this is the way to rock the world! </p>');
+    }
 
+    else  if(req.url == '/about'){
+        res.end('<h1> About code with Harry</h1> <p> Hey this is about code with harry! </p>');
+    }
+    
 })
 
 server.listen(port, ()=>{
