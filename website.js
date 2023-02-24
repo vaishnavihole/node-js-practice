bfb01150614764f26870762441eb0b59c8004445
vaishnavihole <vaishnavihole1@gmail.com>
2023-02-24 14:27:17 +0530

    import hhtp and fs

--- website.js
+++ website.js
@@ -1,4 +1,6 @@
+const { Console } = require('console')
 const http = require('http');
+const fs = require('fs')
 
 const port = process.env.PORT || 3000;
 
@@ -13,6 +15,11 @@ const server = http.createServer((req, res)=>{
 
     else  if(req.url == '/about'){
         res.statusCode = 200;
+        res.end('<h1> About code with Harry</h1> <p> Hey this is about code with harry! </p>');
+    }
+    else  if(req.url == '/hello'){
+        res.statusCode = 200;
+       
         res.end('<h1> About code with Harry</h1> <p> Hey this is about code with harry! </p>');
     }
     else{
