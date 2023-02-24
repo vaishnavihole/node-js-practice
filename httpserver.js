4fb54c0e174f86c8b2173adf91625ac09cd63a31
vaishnavihole <vaishnavihole1@gmail.com>
2023-02-24 10:32:53 +0530

    add port

--- httpserver.js
+++ httpserver.js
@@ -5,6 +5,11 @@ const port =process.env.port;
 const server = http.createServer((req, res)=>{
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html')
-    res.end('<h1> This is Harry</h1> <p> Hey this is the way to rock the world! </p>')
+    res.end('<h1> This is Harry</h1> <p> Hey this is the way to rock the world! </p>');
 
 })
+
+server.listen(port, ()=>{
+    console.log(`Server is listening on portn ${port}`);
+})
+
