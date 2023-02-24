9b1dfdd4a9383b28ccc0bf73ae7c46fc5ea8f8ea
vaishnavihole <vaishnavihole1@gmail.com>
2023-02-24 10:12:43 +0530

    use setTime out

--- nodejsevents.js
+++ nodejsevents.js
@@ -3,7 +3,12 @@ const EventEmitter = require('events');
 class MyEmitter extends EventEmitter {}
 
 const MyEmitter = new MyEmitter();
-MyEmitter.on('event', () => {
-    console.log('an event occurred!');
+MyEmitter.on('WaterFull', () => {
+    console.log('Please turn off motor!');
+    setTimeout(() => {
+        console.log('Please turn off motor!');
+    }, 3000)
 });
-MyEmitter.emit('event');
+
+
+//MyEmitter.emit('event');
