ff7db620c6d77e82ce755132f694727dc91ffd77
vaishnavihole <vaishnavihole1@gmail.com>
2023-02-24 11:37:21 +0530

    use statusCode

--- website.js
+++ website.js
@@ -3,15 +3,21 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res)=>{
-    res.statusCode = 200;
+
     res.setHeader('Content-Type', 'text/html')
     console.log(req.url)
     if(req.url == '/'){
+        res.statusCode = 200;
         res.end('<h1> This is Harry</h1> <p> Hey this is the way to rock the world! </p>');
     }
 
     else  if(req.url == '/about'){
+        res.statusCode = 200;
         res.end('<h1> About code with Harry</h1> <p> Hey this is about code with harry! </p>');
+    }
+    else{
+        res.statusCode = 404;
+
     }
     
 })
